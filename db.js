29bc91fe5ac60273129569cef52428b38b511624
SOFTHELM SERVER <celestial.chancellor@gmail.com>
2025-11-14 13:15:44 +1100

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
+const mysql = require('mysql2/promise');
 require('dotenv').config();
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-  pool
-};
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
