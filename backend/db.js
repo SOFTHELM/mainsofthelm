3bd86b570940287c9a14e01eb1c53c076a1fe646
SOFTHELM SERVER <celestial.chancellor@gmail.com>
2025-11-17 11:18:09 +1100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool({ 
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false } // required for Render
 });
